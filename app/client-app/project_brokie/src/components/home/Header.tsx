7d094f3ba009539c41a09c09d6c6271f44806bf7
faridul haque <faridulhaquemrushed@gmail.com>
2024-06-16 01:34:02 +0600

    navbar responsive view for mobile devices added

--- app/client-app/project_brokie/src/components/home/Header.tsx
+++ app/client-app/project_brokie/src/components/home/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from "@/styles/header.module.css"
 import Navbar from '../shared/Navbar';
+import AppBar from '../shared/AppBar';
 
 const Header = () => {
     return (
         <div className={styles.container}>
            <Navbar></Navbar>
+           <AppBar></AppBar>
         </div>
     );
 };
--- app/client-app/project_brokie/src/components/shared/AppBar.tsx
+++ app/client-app/project_brokie/src/components/shared/AppBar.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
+import styles from '@/styles/appBar.module.css';
+import Image from 'next/image';
+import logo from '@/assets/logo/logo.png';
+import { FaBars, FaRegWindowClose } from 'react-icons/fa';
+import { navItems } from '@/services/constant';
+import { TNavItems } from '@/types/tConstant';
+import Link from 'next/link';
 
 const AppBar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [openDrawer, setOpenDrawer] = useState(false);
+  return (
+    <div className={styles.appBar}>
+      <Image src={logo} width={120} height={60} alt="logo"></Image>
+      {openDrawer ? (
+        <FaRegWindowClose
+          onClick={() => setOpenDrawer(!openDrawer)}
+          className={styles?.icon}
+        ></FaRegWindowClose>
+      ) : (
+        <FaBars
+          onClick={() => setOpenDrawer(!openDrawer)}
+          className={styles?.icon}
+        />
+      )}
+
+      <ul className={openDrawer ? styles.openMenu : styles.closeMenu}>
+        <li className={styles.loginWrapper}>
+            <Link href="">Login</Link>
+        </li>
+        
+        {navItems.map((item: TNavItems) => (
+          <li className={styles.navItem} key={item.title}>
+            <Link href="#">{item.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default AppBar;
