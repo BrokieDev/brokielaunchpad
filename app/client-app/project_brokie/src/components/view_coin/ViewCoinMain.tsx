1efc149699aab9381902eb6b6f8f61d6451a9cd4
faridul haque <faridulhaquemrushed@gmail.com>
2024-06-20 21:36:37 +0600

    middle container left part started

--- app/client-app/project_brokie/src/components/view_coin/ViewCoinMain.tsx
+++ app/client-app/project_brokie/src/components/view_coin/ViewCoinMain.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from '@/styles/viewCoinMain.module.css';
 import ViewCoinTop from './ViewCoinTop';
+import ViewCoinMiddle from './ViewCoinMiddle';
 
 const ViewCoinMain = () => {
   return (
     <div className={styles.mainContainer}>
       <ViewCoinTop></ViewCoinTop>
+      <ViewCoinMiddle></ViewCoinMiddle>
     </div>
   );
 };
---
+++ app/client-app/project_brokie/src/components/view_coin/ViewCoinMiddle.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '@/styles/viewCoinMiddle.module.css';
+
+const ViewCoinMiddle = () => {
+  return (
+    <div className={styles.middleContainer}>
+      <div className={styles.middleContainerLeft}></div>
+      <div className={styles.middleContainerRight}></div>
+    </div>
+  );
+};
+
+export default ViewCoinMiddle;
