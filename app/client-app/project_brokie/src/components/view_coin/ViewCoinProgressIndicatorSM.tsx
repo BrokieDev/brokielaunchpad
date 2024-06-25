62b6892dba87f1cdab1e5aeb98b0ab1f63aaec4e
faridul haque <faridulhaquemrushed@gmail.com>
2024-06-26 00:17:54 +0600

    new responsive file for mobile device for indicator progress part has been added

--- app/client-app/project_brokie/src/components/view_coin/ViewCoinProgressIndicatorSM.tsx
+++ app/client-app/project_brokie/src/components/view_coin/ViewCoinProgressIndicatorSM.tsx
@@ -1,8 +1,46 @@
 import React from 'react';
 import styles from '@/styles/viewCoinProgressIndicatorSM.module.css';
+import { indicators } from '@/services/constant';
+import { TIndicatorsItem } from '@/types/indicator';
 
 const ViewCoinProgressIndicatorSM = () => {
-  return <div className={styles.indicatorsContainer}></div>;
+  return (
+    <div className={styles.indicatorMain}>
+      <div className={styles.indicatorLine}>
+        <div
+          style={{ height: '33%' }}
+          className={styles.indicatorLineSuccess}
+        ></div>
+      </div>
+
+      <div className={styles.indicatorsContainer}>
+        {indicators.map((indicator: TIndicatorsItem, i: number) => (
+          <div key={i} className={styles.indicator}>
+            <div className={styles.indicatorCircleWrapper}>
+              <div
+                className={`${styles.indicatorCircle} ${''}
+          ${
+            indicator.active
+              ? styles.indicatorCircleActive
+              : styles.indicatorCircleInactive
+          }
+          `}
+              >
+                <div>
+                  <span></span>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.indicatorText}>
+              <h3>{indicator.title}</h3>
+              <h5>{indicator.subtitle} </h5>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ViewCoinProgressIndicatorSM;
