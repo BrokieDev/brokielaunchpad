a52cd30c104a20618337ed00ef44a9ec42e910c0
faridul haque <faridulhaquemrushed@gmail.com>
2024-06-25 18:21:05 +0600

    new indicator progress section for view_coin page for larger devices has been added

--- app/client-app/project_brokie/src/components/view_coin/ViewCoinProgressIndicatorLG.tsx
+++ app/client-app/project_brokie/src/components/view_coin/ViewCoinProgressIndicatorLG.tsx
@@ -5,31 +5,36 @@ import { indicators } from '@/services/constant';
 
 const ViewCoinProgressIndicatorLG = () => {
   return (
-    <div className={styles.indicatorsContainer}>
-      {indicators.map((indicator: TIndicatorsItem, i: number) => (
-        <div key={i} className={styles.indicator}>
-          <div
-            className={`${styles.indicatorCircle} ${
-              !indicator.lastChild ? styles.indicatorLine : ''
-            }
+    <div className={styles.indicatorMain}>
+      <div className={styles.indicatorLine}>
+        <div 
+        style={{width: '99%',}}
+        className={styles.indicatorLineSuccess}></div>
+      </div>
+      <div className={styles.indicatorsContainer}>
+        {indicators.map((indicator: TIndicatorsItem, i: number) => (
+          <div key={i} className={styles.indicator}>
+            <div
+              className={`${styles.indicatorCircle} ${''}
           ${
             indicator.active
               ? styles.indicatorCircleActive
               : styles.indicatorCircleInactive
           }
           `}
-          >
-            <div>
-              <span></span>
+            >
+              <div>
+                <span></span>
+              </div>
+            </div>
+
+            <div className={styles.indicatorText}>
+              <h3>{indicator.title}</h3>
+              <h5>{indicator.subtitle} </h5>
             </div>
           </div>
-
-          {/* <div className={styles.indicatorText}>
-          <h3>{indicator.title}</h3>
-          <h5>{indicator.subtitle} </h5>
-        </div> */}
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
