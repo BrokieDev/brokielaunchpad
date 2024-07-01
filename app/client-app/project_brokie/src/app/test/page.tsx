47eff3e138f0fd6e80bb0e0513b2b41ca0a26bea
faridul haque <faridulhaquemrushed@gmail.com>
2024-07-01 12:45:54 +0600

    test page and component ready

---
+++ app/client-app/project_brokie/src/app/test/page.tsx
@@ -0,0 +1,12 @@
+import TestWalletIntegration from '@/components/test/TestWalletIntegration';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <TestWalletIntegration></TestWalletIntegration>
+        </div>
+    );
+};
+
+export default page;
---
+++ app/client-app/project_brokie/src/components/test/TestWalletIntegration.tsx
@@ -0,0 +1,47 @@
+'use client';
+import { TonConnectButton, useTonWallet } from '@tonconnect/ui-react';
+import React from 'react';
+
+const TestWalletIntegration = () => {
+  const wallet = useTonWallet();
+  console.log(wallet);
+
+  const handleTest = async () => {};
+
+  return (
+    <div>
+      <h2 style={{ textAlign: 'center', paddingTop: '30px' }}>
+        Hello test wallet
+      </h2>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <TonConnectButton className="my-button-class"></TonConnectButton>
+        <br />
+        <hr />
+        <br />
+
+        <button
+          style={{
+            padding: '15px 35px',
+            backgroundColor: 'green',
+            color: 'white',
+            border: 'none',
+            borderRadius:'5px',
+            cursor: 'pointer'
+          }}
+          onClick={() => handleTest()}
+        >
+          Test Now
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TestWalletIntegration;
